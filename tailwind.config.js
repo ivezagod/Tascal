181feb1bffd0580e76620d21a667778de8999ecc
ivezagod <nemanjaivezic1@gmail.com>
2024-05-10 17:22:11 +0200

    fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [ "./index.html",
+      "about.html",
+      "commissioning&startup.html",
+      "consulting.html",
+      "controlPanelsProduction.html",
+      "electricalSystemDesign.html",
+      "installation.html",
+      "maintenance.html",
+      "programing.html",
+      "app.js",
+      "aboutus.js",
     "{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'logo': "url('/media/logo/Tascal-Logo@4x.png')",
-        'hero':"url('/media/random/slika.png')"
+        'logo': "url('./media/logo/Tascal-Logo@4x.png')",
+        'hero':"url('./media/random/slika.png')"
 
       },
       colors: {
