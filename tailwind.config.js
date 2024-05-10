7f9b1ce200131038a8b0f0da7e5122f72bf3d10a
ivezagod <nemanjaivezic1@gmail.com>
2024-05-10 17:13:32 +0200

    changed paths to images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'logo': "url('/media/logo/Tascal-Logo@4x.png')",
+        'hero':"url('../media/random/slika.png')"
+
+      },
+      colors: {
+        'dark-blue': '#000000',
+
+      },
+      maxWidth:{
+        container: "1440px"
+      },
+      fontFamily: {
+        'main': ['montserrat']
+      }
+    }
   },
   plugins: [],
 }
